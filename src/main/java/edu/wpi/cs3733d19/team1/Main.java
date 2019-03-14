143d4558f5bc028af68c81b7283682285b1fa724
Remy Allegro <rallegro@wpi.edu>
2019-03-14 12:42:37 -0400

    My name now

--- src/main/java/edu/wpi/cs3733d19/team1/Main.java
+++ src/main/java/edu/wpi/cs3733d19/team1/Main.java
@@ -2,6 +2,6 @@ package edu.wpi.cs3733d19.team1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Remy Allegro");
     }
 }
